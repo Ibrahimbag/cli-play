6929effe0e9b9c62237800cd3f47940dfe04057f
İbrahim Bağ <ibrahimbag33@outlook.com>
2025-05-23 14:19:23 +0300

    Refactor functions into cli_play class

--- cli-play.py
+++ cli-play.py
@@ -5,52 +5,58 @@ from time import sleep
 from ffpyplayer.player import MediaPlayer
 
 
-def play_video(term):
-    cap = cv2.VideoCapture(argv[1])
-    player = MediaPlayer(argv[1])
+class cli_play:
+    def __init__(self):
+        self.term = Terminal()
 
-    if not cap.isOpened():
-        print("Error: Could not open video.")
-        exit(3)
+    def play_video(self, file_name):
+        cap = cv2.VideoCapture(file_name)
+        player = MediaPlayer(file_name)
 
-    while term.inkey(timeout=0) != "q":
-        success, image = cap.read()
-        if not success:
-            break
+        if not cap.isOpened():
+            print("Error: Could not open video.")
+            exit(3)
 
-        height, width = term.height, term.width
-        image = cv2.resize(image, (width, height), interpolation=cv2.INTER_AREA)
+        while self.term.inkey(timeout=0) != "q":
+            success, image = cap.read()
+            if not success:
+                break
 
-        output = []
-        for y in range(height):
-            for x in range(width):
-                b, g, r = image[y, x]
-                output.append(term.move(y, x) + term.on_color_rgb(r, g, b) + " ")
+            height, width = self.term.height, self.term.width
+            image = cv2.resize(image, (width, height), interpolation=cv2.INTER_AREA)
 
-        print("".join(output))
+            output = []
+            for y in range(height):
+                for x in range(width):
+                    b, g, r = image[y, x]
+                    output.append(
+                        self.term.move(y, x) + self.term.on_color_rgb(r, g, b) + " "
+                    )
 
-        fps = 1 / cap.get(cv2.CAP_PROP_FPS)
-        audio_frame, val = player.get_frame()
-        if val != "eof" and audio_frame is not None:
-            _, pts = audio_frame
-            delay = fps - (player.get_pts() - pts)
-            sleep(max(0, delay))
-        else:
-            sleep(fps)
+            print("".join(output))
 
-    cap.release()
+            fps = 1 / cap.get(cv2.CAP_PROP_FPS)
+            audio_frame, val = player.get_frame()
+            if val != "eof" and audio_frame is not None:
+                _, pts = audio_frame
+                delay = fps - (player.get_pts() - pts)
+                sleep(max(0, delay))
+            else:
+                sleep(fps)
+
+        cap.release()
 
 
 def main():
     try:
-        argv[1]
+        file_name = argv[1]
     except IndexError:
         print(f"Usage: {argv[0]} [FILE_NAME]")
         exit(1)
 
-    term = Terminal()
-    with term.fullscreen(), term.cbreak(), term.hidden_cursor():
-        play_video(term)
+    app = cli_play()
+    with app.term.fullscreen(), app.term.cbreak(), app.term.hidden_cursor():
+        app.play_video(file_name)
 
 
 if __name__ == "__main__":
