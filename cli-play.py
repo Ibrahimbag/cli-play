0e5be85d3afb9fa695ef2d256149d9aa8aef75d1
İbrahim Bağ <ibrahimbag33@outlook.com>
2025-05-23 15:36:40 +0300

    Add image displaying

--- cli-play.py
+++ cli-play.py
@@ -3,19 +3,32 @@ import cv2
 from sys import argv, exit
 from time import sleep
 from ffpyplayer.player import MediaPlayer
+from pymediainfo import MediaInfo
 
 
 class cli_play:
     def __init__(self):
         self.term = Terminal()
 
-    def play_video(self, file_name):
-        cap = cv2.VideoCapture(file_name)
-        player = MediaPlayer(file_name)
+    def run(self, file_path):
+        try:
+            file_info = MediaInfo.parse(file_path)
+        except:
+            exit("file not found")
+
+        with self.term.fullscreen(), self.term.cbreak(), self.term.hidden_cursor():
+            for track in file_info.tracks:
+                if track.track_type == "Video":
+                    self.play_video(file_path)
+                elif track.track_type == "Image":
+                    self.show_picture(file_path)
+
+    def play_video(self, file_path):
+        cap = cv2.VideoCapture(file_path)
+        player = MediaPlayer(file_path)
 
         if not cap.isOpened():
-            print("Error: Could not open video.")
-            exit(3)
+            exit("Error: Could not open video.")
 
         while self.term.inkey(timeout=0) != "q":
             success, image = cap.read()
@@ -46,17 +59,42 @@ class cli_play:
 
         cap.release()
 
+    def show_picture(self, file_path):
+        image = cv2.imread(file_path)
+        if image is None:
+            exit("Could not read the image")
+
+        old_height, old_width = 0, 0
+        while self.term.inkey(timeout=0) != "q":
+            height, width = self.term.height, self.term.width
+            if height != old_height or width != old_width:
+                image = cv2.imread(file_path)
+                image = cv2.resize(image, (width, height), interpolation=cv2.INTER_AREA)
+
+            output = []
+            for y in range(height):
+                for x in range(width):
+                    b, g, r = image[y, x]
+                    output.append(
+                        self.term.move(y, x) + self.term.on_color_rgb(r, g, b) + " "
+                    )
+
+            if height != old_height or width != old_width:
+                print("".join(output))
+                old_height, old_width = height, width
+
+            sleep(1)
+
 
 def main():
     try:
-        file_name = argv[1]
+        file_path = argv[1]
     except IndexError:
-        print(f"Usage: {argv[0]} [FILE_NAME]")
+        print(f"Usage: {argv[0]} [file_path]")
         exit(1)
 
     app = cli_play()
-    with app.term.fullscreen(), app.term.cbreak(), app.term.hidden_cursor():
-        app.play_video(file_name)
+    app.run(file_path)
 
 
 if __name__ == "__main__":
